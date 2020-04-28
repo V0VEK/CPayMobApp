e31931487efc94ce8ac06baa56946ee7d9e00a88
Владимир Ковалев <volodya.vladimir.srg@gmail.com>
2020-04-28 23:58:09 +0300

    maybe skeleton finished

--- app/src/main/java/com/ibks/cpay/authentication/SignInActivity.java
+++ app/src/main/java/com/ibks/cpay/authentication/SignInActivity.java
@@ -2,7 +2,9 @@ package com.ibks.cpay.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ibks.cpay.R;
 
@@ -12,5 +14,18 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void SendConfirmationCodeClicked (View v) {
+        // TODO: Send request with confirmation code and check response for starting user adding activity
+
+        StartUserCreationActivity();
+    }
+
+
+    private void StartUserCreationActivity() {
+        Intent intent = new Intent(this, UserCreationActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/ibks/cpay/authentication/UserCreationActivity.java
@@ -0,0 +1,30 @@
+package com.ibks.cpay.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.ibks.cpay.R;
+
+public class UserCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_creation);
+    }
+
+    public void UserCreateButtonClicked(View v) {
+        // TODO: Send request to the backend and check response
+        
+        StartLoginActivity();
+    }
+
+    private void StartLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
